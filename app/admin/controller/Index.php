7cf851701cc841f53dc5fd75a5042fd82ccab9b8
Jason <jasonwell@139.com>
2018-02-23 08:22:07 +0800

    【开发】后台首页行为

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,31 @@
+<?php
+// +----------------------------------------------------------------------
+// | HeilPHP
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://www.heilphp.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Jason <1878566968@qq.com>
+// +----------------------------------------------------------------------
+
+namespace app\admin\Controller;
+
+use Hook;
+use app\admin\controller\Admin;
+
+/**
+ * 后台首页控制器
+ * @author Jason	<1878566968@qq.com>
+ */
+class Index extends Admin{
+
+    /**
+     * 后台首页
+     * @author Jason	<1878566968@qq.com>
+     */
+    public function index(){
+        $this->assign('meta_title','管理首页');
+		dump(Hook::listen('admin_index'));
+		//return view();
+    }
+
+}
