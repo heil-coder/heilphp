15a70fe68bde75504e2806c48c4d59e03bcec032
Jason <jasonwell@139.com>
2018-02-28 09:19:39 +0800

    【开发】后台配置管理开发

--- app/admin/controller/Config.php
+++ app/admin/controller/Config.php
@@ -41,6 +41,71 @@ class Config extends Admin{
         $this->assign('meta_title', '配置管理');
 		return view();
     }
+    /**
+     * 新增配置
+     */
+    //public function add(){
+    //    if(IS_POST){
+    //        $Config = D('Config');
+    //        $data = $Config->create();
+    //        if($data){
+    //            if($Config->add()){
+    //                S('DB_CONFIG_DATA',null);
+    //                $this->success('新增成功', U('index'));
+    //            } else {
+    //                $this->error('新增失败');
+    //            }
+    //        } else {
+    //            $this->error($Config->getError());
+    //        }
+    //    } else {
+    //        $this->meta_title = '新增配置';
+    //        $this->assign('info',null);
+    //        $this->display('edit');
+    //    }
+    //}
+    /**
+     * 编辑配置
+     */
+    public function edit($id = 0){
+        if(Request::isPost()){
+            $dbConfig = db('Config');
+            $data = Request::param();
+            if($data){
+				if(empty($data['id'])){
+					$result = $dbConfig->insert($data);
+				}
+				else{
+					$result = $dbConfig->update($data);
+				}
+                if($result === false){
+                    $this->error('更新失败');
+                } else {
+                    cache('DB_CONFIG_DATA',null);
+                    //记录行为
+                    //action_log('update_config','config',$data['id'],UID);
+                    $this->success('更新成功', Cookie('__forward__'));
+                }
+            } else {
+                $this->error($dbConfig->getError());
+            }
+        } else {
+            $info = array();
+            /* 获取数据 */
+			if(!empty($id)){
+				$info = db('Config')->field(true)->find($id);
+				if(empty($info)){
+					$this->error('获取配置信息错误');
+				}
+				$this->assign('info', $info);
+			}
+
+			$this->assign('typeList',get_config_type());
+			$this->assign('groupList',get_config_group());
+			$this->assign('meta_title', '编辑配置');
+			return view();
+        }
+    }
     // 获取某个标签的配置参数
     public function group() {
         $id     =   Request::get('id/d',1);
@@ -69,4 +134,5 @@ class Config extends Admin{
         cache('DB_CONFIG_DATA',null);
         $this->success('保存成功！');
     }
+
 }
