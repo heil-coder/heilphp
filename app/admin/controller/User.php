558972eced9d8124723e29cf35a6415e00332cb1
Jason <jasonwell@139.com>
2018-03-07 07:13:42 +0800

    【开发】用户管理开发

--- app/admin/controller/User.php
+++ app/admin/controller/User.php
@@ -10,6 +10,7 @@
 namespace app\admin\controller;
 use Request;
 use app\admin\controller\Admin;
+use app\user\api\UserApi;
 
 /**
  * 用户管理控制器
@@ -23,8 +24,8 @@ class User extends Admin{
     public function index(){
         $nickname       =   Request::get('nickname');
 		$this->assign('nickname',$nickname);
-		$map = array();
-        $map[]  =   array('status','>=',0);
+		$map = [];
+        $map[]  =   ['status','>=',0];
         if(is_numeric($nickname)){
             //$map['uid|nickname']=   array(intval($nickname),array('like','%'.$nickname.'%'),'_multi'=>true);
         }else{
@@ -38,5 +39,30 @@ class User extends Admin{
         $this->assign('meta_title', $this->meta_title);
 
 		return view();
+    }
+    public function add($username = '', $password = '', $repassword = '', $email = ''){
+        if(Request::isPost()){
+            /* 检测密码 */
+            if($password != $repassword){
+                $this->error('密码和重复密码不一致！');
+            }
+
+            /* 调用注册接口注册用户 */
+            $User   =   new UserApi;
+            $uid    =   $User->register($username, $password, $email);
+            if(0 < $uid){ //注册成功
+                $user = array('uid' => $uid, 'nickname' => $username, 'status' => 1);
+                if(!db('Member')->add($user)){
+                    $this->error('用户添加失败！');
+                } else {
+                    $this->success('用户添加成功！',Url('index'));
+                }
+            } else { //注册失败，显示错误信息
+                $this->error($this->showRegError($uid));
+            }
+        } else {
+            $this->assign('meta_title','新增用户');
+			return view();
+        }
     }
 }
