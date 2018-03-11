30cdd891ee4b75670ceea84a87c07e4350f70687
Jason <jasonwell@139.com>
2018-03-12 07:00:42 +0800

    【开发】模型编辑模板页面

--- app/admin/common.php
+++ app/admin/common.php
@@ -167,3 +167,27 @@ function get_action($id = null, $field = null){
     }
     return empty($field) ? $list[$id] : $list[$id][$field];
 }
+ // 分析枚举类型字段值 格式 a:名称1,b:名称2
+ // 暂时和 parse_config_attr功能相同
+ // 但请不要互相使用，后期会调整
+function parse_field_attr($string) {
+    if(0 === strpos($string,':')){
+        // 采用函数定义
+        return   eval('return '.substr($string,1).';');
+    }elseif(0 === strpos($string,'[')){
+        // 支持读取配置参数（必须是数组类型）
+        return config(substr($string,1,-1));
+    }
+    
+    $array = preg_split('/[,;\r\n]+/', trim($string, ",;\r\n"));
+    if(strpos($string,':')){
+        $value  =   array();
+        foreach ($array as $val) {
+            list($k, $v) = explode(':', $val);
+            $value[$k]   = $v;
+        }
+    }else{
+        $value  =   $array;
+    }
+    return $value;
+}
--- app/admin/controller/Modelmanage.php
+++ app/admin/controller/Modelmanage.php
@@ -41,6 +41,56 @@ class Modelmanage extends Admin {
 		return view();
     }
     /**
+     * 编辑页面初始化
+     */
+    public function edit(){
+        $id = Request::param('id/d','');
+        if(empty($id)){
+            $this->error('参数不能为空！');
+        }
+
+        /*获取一条记录的详细数据*/
+        $Model = model('Modelmanage');
+        $data = $Model->field(true)->find($id);
+        if(!$data){
+            $this->error($Model->getError());
+        }
+        $data['attribute_list'] = empty($data['attribute_list']) ? '' : explode(",", $data['attribute_list']);
+        $fields = db('Attribute')->where('model_id',$data['id'])->column('id,name,title,is_show');
+        $fields = empty($fields) ? array() : $fields;
+        // 是否继承了其他模型
+        if($data['extend'] != 0){
+            $extend_fields  = db('Attribute')->where('model_id',$data['extend'])->column('id,name,title,is_show');
+            $fields        += $extend_fields;
+        }
+        
+        // 梳理属性的可见性
+        foreach ($fields as $key=>$field){
+            if (!empty($data['attribute_list']) && !in_array($field['id'], $data['attribute_list'])) {
+                $fields[$key]['is_show'] = 0;
+            }
+        }
+        
+        // 获取模型排序字段
+        $field_sort = json_decode($data['field_sort'], true);
+        if(!empty($field_sort)){
+            foreach($field_sort as $group => $ids){
+                foreach($ids as $key => $value){
+                    $fields[$value]['group']  =  $group;
+                    $fields[$value]['sort']   =  $key;
+                }
+            }
+        }
+        
+        // 模型字段列表排序
+        $fields = list_sort_by($fields,"sort");
+        
+        $this->assign('fields', $fields);
+        $this->assign('info', $data);
+        $this->assign('meta_title','编辑模型');
+		return view();
+    }
+    /**
      * 更新一条数据
      */
     public function update(){
