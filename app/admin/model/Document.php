301e09411c71d450df6791070a7b01e31098cb68
Jason <jasonwell@139.com>
2018-03-21 16:51:02 +0800

    【开发】更多用户行为加入，新增文档行为日志记录开启

--- app/admin/model/Document.php
+++ app/admin/model/Document.php
@@ -147,7 +147,7 @@ class Document extends Model{
 
         //行为记录
         if($id){
-            //action_log('add_document', 'document', $id, UID);
+            action_log('add_document', 'document', $id, UID);
         }
 
         //内容添加或更新完成
--- app/install/data/install.sql
+++ app/install/data/install.sql
@@ -360,12 +360,17 @@ CREATE TABLE `heilphp_action` (
 -- -----------------------------
 -- Records of `heilphp_action`
 -- -----------------------------
-INSERT INTO `heilphp_action` (`id`, `name`, `title`, `remark`, `rule`, `log`, `type`, `status`, `update_time`, `delete_time`) VALUES
-(1, 'user_login', '用户登录', '积分+10，每天一次', 'table:member|field:score|condition:id={$self} AND status>-1|rule:score+10|cycle:24|max:1;', '[user|get_nickname]在[time|time_format]登录了后台', 1, 1, 1520551087, NULL),
-(2, 'update_config', '更新配置', '新增或修改或删除配置', '', '', 1, 1, 1520551087, NULL),
-(3, 'update_model', '更新模型', '新增或修改模型', '', '', 1, 1, 1520551087, NULL),
-(4, 'update_channel', '更新导航', '新增或修改或删除导航', '', '', 1, 1, 1520551087, NULL),
-(5, 'update_menu', '更新菜单', '新增或修改或删除菜单', '', '', 1, 1, 1520551087, NULL);
+INSERT INTO `heilphp_action` VALUES ('1', 'user_login', '用户登录', '积分+10，每天一次', 'table:member|field:score|condition:id={$self} AND status>-1|rule:score+10|cycle:24|max:1;', '[user|get_nickname]在[time|time_format]登录了后台', '1', '1', '1387181220',null);
+INSERT INTO `heilphp_action` VALUES ('2', 'add_article', '发布文章', '积分+5，每天上限5次', 'table:member|field:score|condition:id={$self}|rule:score+5|cycle:24|max:5', '', '2', '0', '1380173180',null);
+INSERT INTO `heilphp_action` VALUES ('3', 'review', '评论', '评论积分+1，无限制', 'table:member|field:score|condition:id={$self}|rule:score+1', '', '2', '1', '1383285646',null);
+INSERT INTO `heilphp_action` VALUES ('4', 'add_document', '发表文档', '积分+10，每天上限5次', 'table:member|field:score|condition:id={$self}|rule:score+10|cycle:24|max:5', '[user|get_nickname]在[time|time_format]发表了一篇文章。\r\n表[model]，记录编号[record]。', '2', '0', '1386139726',null);
+INSERT INTO `heilphp_action` VALUES ('5', 'add_document_topic', '发表讨论', '积分+5，每天上限10次', 'table:member|field:score|condition:id={$self}|rule:score+5|cycle:24|max:10', '', '2', '0', '1383285551',null);
+INSERT INTO `heilphp_action` VALUES ('6', 'update_config', '更新配置', '新增或修改或删除配置', '', '', '1', '1', '1383294988',null);
+INSERT INTO `heilphp_action` VALUES ('7', 'update_model', '更新模型', '新增或修改模型', '', '', '1', '1', '1383295057',null);
+INSERT INTO `heilphp_action` VALUES ('8', 'update_attribute', '更新属性', '新增或更新或删除属性', '', '', '1', '1', '1383295963',null);
+INSERT INTO `heilphp_action` VALUES ('9', 'update_channel', '更新导航', '新增或修改或删除导航', '', '', '1', '1', '1383296301',null);
+INSERT INTO `heilphp_action` VALUES ('10', 'update_menu', '更新菜单', '新增或修改或删除菜单', '', '', '1', '1', '1383296392',null);
+INSERT INTO `heilphp_action` VALUES ('11', 'update_category', '更新分类', '新增或修改或删除分类', '', '', '1', '1', '1383296765',null);
 
 -- -----------------------------
 -- Table structure for `heilphp_action_log`
