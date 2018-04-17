0e9eb598060f3c0dc157669568097c2678316ec7
Jason <jasonwell@139.com>
2018-04-17 14:32:17 +0800

    【开发】模型数据新增，自动完成未处理

--- application/admin/controller/Think.php
+++ application/admin/controller/Think.php
@@ -16,6 +16,7 @@
 // +----------------------------------------------------------------------
 
 namespace app\admin\controller;
+use think\Validate;
 
 /**
  * 模型数据管理控制器
@@ -213,10 +214,10 @@ class Think extends Admin{
             $Model  =   model(parse_name(get_table_name($model['id']),1));
             // 获取模型的字段信息
             $Model  =   $this->checkAttr($Model,$model['id']);
-            if($Model->create() && $Model->add()){
+			if(is_object($Model) && $Model->save()){
                 $this->success('添加'.$model['title'].'成功！', Url('lists?model='.$model['name']));
             } else {
-                $this->error($Model->getError());
+                $this->error($Model);
             }
         } else {
 
@@ -232,25 +233,36 @@ class Think extends Admin{
     protected function checkAttr($Model,$model_id){
         $fields     =   get_model_attribute($model_id,false);
         $validate   =   $auto   =   array();
+		$rule = $msg = [];
         foreach($fields as $key=>$attr){
             if($attr['is_must']){// 必填字段
-                $validate[]  =  array($attr['name'],'require',$attr['title'].'必须!');
+				$rule[$attr['name']][]  =  'require';
+				$msg[$attr['name'].'.require'] = $attr['title'].'必须!';
             }
             // 自动验证规则
             if(!empty($attr['validate_rule'])) {
-                $validate[]  =  array($attr['name'],$attr['validate_rule'],$attr['error_info']?$attr['error_info']:$attr['title'].'验证错误',0,$attr['validate_type'],$attr['validate_time']);
+                //$validate[]  =  array($attr['name'],$attr['validate_rule'],$attr['error_info']?$attr['error_info']:$attr['title'].'验证错误',0,$attr['validate_type'],$attr['validate_time']);
+				$rule[$attr['name']][] = $attr['validate_rule'];
+				$msg[$attr['name'].'.'.$attr['validate_rule']] = $attr['error_info']?$attr['error_info']:$attr['title'].'验证错误';
             }
             // 自动完成规则
-            if(!empty($attr['auto_rule'])) {
-                $auto[]  =  array($attr['name'],$attr['auto_rule'],$attr['auto_time'],$attr['auto_type']);
-            }elseif('checkbox'==$attr['type']){ // 多选型
-                $auto[] =   array($attr['name'],'arr2str',3,'function');
-            }elseif('date' == $attr['type']){ // 日期型
-                $auto[] =   array($attr['name'],'strtotime',3,'function');
-            }elseif('datetime' == $attr['type']){ // 时间型
-                $auto[] =   array($attr['name'],'strtotime',3,'function');
-            }
+            //if(!empty($attr['auto_rule'])) {
+            //    $auto[]  =  array($attr['name'],$attr['auto_rule'],$attr['auto_time'],$attr['auto_type']);
+            //}elseif('checkbox'==$attr['type']){ // 多选型
+            //    $auto[] =   array($attr['name'],'arr2str',3,'function');
+            //}elseif('date' == $attr['type']){ // 日期型
+            //    $auto[] =   array($attr['name'],'strtotime',3,'function');
+            //}elseif('datetime' == $attr['type']){ // 时间型
+            //    $auto[] =   array($attr['name'],'strtotime',3,'function');
+            //}
         }
-        return $Model->validate($validate)->auto($auto);
+		$validate = Validate::make($rule,$msg);
+		$data = Request()->param();
+		if(!$validate->check($data)){
+			return $validate->getError();
+		}
+		else{
+			return $Model->data($data);
+		}
     }
 }
