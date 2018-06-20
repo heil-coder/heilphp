d91e08af73ff41d2491c2d7d96327f10093452f7
Jason <jasonwell@139.com>
2018-06-20 20:40:15 +0800

    【开发】模型新增验证优化

--- application/admin/validate/Model.php
+++ application/admin/validate/Model.php
@@ -17,12 +17,12 @@ class Model extends Validate
 			'require'														//不能为空
 			,'alpha'														//只能是字母
 			,'length'				=> '3,30'								//长度不合法
-			,'unique'				=> 'ucenter_member,name'				//被占用
+			,'unique'				=> 'model,name'				//被占用
 		]
 		,'title'	=>	[
 			'require'														//不能为空
 			,'chsDash'														//只能是汉字、字母、数字、_、-
-			,'length'				=> '3,30'								//长度不合法	
+			,'length'				=> '2,30'								//长度不合法	
 		]
     ];
 
@@ -34,7 +34,7 @@ class Model extends Validate
 
 		,'title.require'					=> '模型名称不能为空'
 		,'title.chsDash'					=> '模型名称只能是汉字、字母、数字、_、-'
-		,'title.length'						=> '模型名称长度需在3～30个字符之间'
+		,'title.length'						=> '模型名称长度需在3～30之间'
     ];
 	protected function sceneAdd(){
     	return $this->only(['name','title']);
