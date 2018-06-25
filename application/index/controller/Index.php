ff4cb1dff93fe012fcd4814ed08cfd1c96ed6e36
Jason <jasonwell@139.com>
2018-06-25 11:35:43 +0800

    【开发】index模块加入

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,18 @@
 <?php
 namespace app\index\controller;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
     public function index()
     {
+
+        $category = model('Category')->getTree();
+        $lists    = model('Document')->lists(null);
+
+        $this->assign('category',$category);//栏目
+        $this->assign('lists',$lists);//列表
+        $this->assign('page',model('Document')->page);//分页
 		return view();
     }
 }
