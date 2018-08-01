b06d45eb40218227b7d5e1eb824efcbda06bc8f3
Jason <jasonwell@139.com>
2018-08-01 10:20:34 +0800

    【开发】安装模块系统环境检测php版本条件调整

--- application/install/common.php
+++ application/install/common.php
@@ -17,7 +17,7 @@ define('IS_WRITE',true);		//检查是否sae模式
 function check_env(){
     $items = array(
         'os'      => array('操作系统', '不限制', '类Unix', PHP_OS, 'success'),
-        'php'     => array('PHP版本', '5.5', '5.5+', PHP_VERSION, 'success'),
+        'php'     => array('PHP版本', '5.6', '5.6+', PHP_VERSION, 'success'),
         'upload'  => array('附件上传', '不限制', '2M+', '未知', 'success'),
         'gd'      => array('GD库', '2.0', '2.0+', '未知', 'success'),
         'disk'    => array('磁盘空间', '5M', '不限制', '未知', 'success'),
